d01348c435380bdc2fd28881f2e0c267f5e7e639
emywizy <157773880+emywizy@users.noreply.github.com>
2024-06-06 18:20:28 -0500

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jendareyprojectbucketone"
+    bucket = "judebucket23"
     key    = "terraform/backend/eks/terraform.tfstate"
     region = "us-east-1"
   }
